2fc3d5b3277f32e8803435124a2f3804bd53c785
Jonathan Gregson <jdgregson@gmail.com>
2025-06-02 22:16:44 -0400

    Added Google Analytics.

--- slopnews-website/src/ui.js
+++ slopnews-website/src/ui.js
@@ -9,6 +9,15 @@ import {
 export const UI_HTML = `<!DOCTYPE html>
 <html lang="en">
 <head>
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-TPNEVYDVH8"></script>
+    <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'G-TPNEVYDVH8');
+    </script>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>SLOP News 24/7</title>
